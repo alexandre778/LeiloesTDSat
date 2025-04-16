460da1ea9c208421aecf6cca37f63f1b789ed723
“Alexandre <alexgmax@gmail.com>
2025-04-15 23:58:51 -0300

    Botão de salvar cadastrar

--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -183,6 +183,7 @@ public class cadastroVIEW extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
+                System.out.println("Tela ");
                 new cadastroVIEW().setVisible(true);
             }
         });
